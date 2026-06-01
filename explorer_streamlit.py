2888b92d4d66fc3d15c8c35218a7ac95f9a8f040
Jiatong Wang (Alex) <alexjiatongwang@proton.me>
2026-06-01 20:07:48 +0200

    Update explorer_streamlit.py

--- explorer_streamlit.py
+++ explorer_streamlit.py
@@ -875,18 +875,33 @@ with st.expander("Click to View Site Instructions / Welcome Text", expanded=Fals
     ## How to Use
 
     ### Keyword Search
-    * Use the top bar to search for inscriptions using a keyword or phrase. The report printed using the word or phrase search is very minimalist by design.
-
-    ### Full Inscription Reports:
-    * Includes further metadata and a detailed object biography documenting all inscriptions on the object, and all interventions to the each inscription, and the target, extent and method of each intervention. Please generate the full report by copying the inscription ID into the corresponding field and pressing Enter.
-
+    * Use the top bar to search for inscriptions using a keyword or phrase. This report contains some relevant metadata.
+    * In addition, it also records interventions to the each inscription, and the target, extent and method of each intervention.
+    * Every individual mentioned in an inscription is hyperlinked to their database internal record. Click on the link to explore!
+    * Legal reasons aside, since this entire database is smaller than one photograph, we do not host photographs. Almost every inscription is linked to an EDCS record which provides a photo when available.
+    * For more information on the ancient place next to which an inscription is discovered, click the hyperlink to Pleiades, the ancient world gazzetter.
+    * Over 80 percent of inscriptions in this corpus are milestones, for more information on the road segment a milestone belonged to, click the hyperlinked road segment to see its page on the itiner-e project.
+    
     ### Person Reports:
     * This database tracks not only the attested office, group affiliation (e.g. to specific military units, priestly colleges etc.), and social status designation (specifically rangtitels and titles like consularis) of every individual appearing within the corpus but also in which inscriptions the information is attested. Generate a detailed prosopography report by copying a person's ID into the corresponding field and pressing Enter. You can also search for a person's ID using the Person Name field.
 
+    ### EDCS number
+    * Have an EDCS record in mind? You can generate an inscription report using that as well, provided we have the inscription.
+    * Insert the EDCS record number,formatted as EDCS-12345678 and click Generate Report
+    
+    ### Lookup Person ID by Name
+    * Want to search for a specific individual without using the very helpful main search bar for some reason? Be my guest. 
+    * Insert the person's name, click the button Person Name, and look at the Select Person field to the right
+    * SELECT THE DESIRED INVIDIDUAL before clicking the button Generate Report
+    
+    * PLEASE SELECT AN INDIVIDUAL BEFORE GENERATING REPORT. THE DEFAULT INDIVIDUAL AT THE TOP OF THE SELECT BAR IS NOT GUARANTEED TO BE WHO YOU HAVE IN MIND. 
+    
     ### Interactive Map
     * Loading the map may take a second due to the size of the itiner-e roads layer.
-    * Press the Show on Map button after your search or your person / inscription report generation to see all relevant inscriptions on an interactive map.
+    * YOU NEED TO MANUALLY PRESS THE GENERATE MAP BUTTON EVERY TIME after your search or your person / inscription report generation to see all relevant inscriptions on an interactive map.
     * You can click a dot on the map to view its details.
+    * In all applicable cases the EDCS record is hyperlinked.
+    * Similarly, so is the Pleiades record on the area in which the inscription is found.
     * For all inscriptions on milestones, the details popup notes the inscription is on a milestone, names the road segment it served, and contains a link to said road segment on the itiner-e project. Sometimes itiner-e may show you a welcome screen. Click the Explore Roman Roads button to continue to the linked segment. Click the Details button on the left to see more information about the road segment (once on the itiner-e project).
     * For all inscriptions on other inscribed objects, titulorum distributio (type of inscription) and type of support are displayed instead of road information in the details popup.
     * Some objects bear multiple inscriptions. The details popup tells you the number of inscriptions on the support of the inscription and the sequence ID of the inscription you selected. A sequence ID of 1 means the inscription was the earliest inscription on the object, 2 means it was the second inscription on the object etc.
