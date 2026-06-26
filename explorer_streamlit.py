68a52d6afbadd75df259f808aa16a85358a91240
Jiatong Wang (Alex) <alexjiatongwang@proton.me>
2026-06-26 17:19:44 +0200

    Update explorer_streamlit.py

--- explorer_streamlit.py
+++ explorer_streamlit.py
@@ -2234,7 +2234,7 @@ with st.expander("Close / Open Interactive Leaflet Map Layer Visualizer", expand
     if st.session_state.get("trigger_map_html"):
         st.components.v1.html(st.session_state.trigger_map_html, height=700, scrolling=True)
     else:
-        st.info("No map generated yet. If you have yet to make a search, do so. Then click 'Generate Map' to plot inscriptions matching your query a map.")
+        st.info("No map generated yet. If you have yet to make a search, do so. Then the 'Generate Map' button will appear. Click it to plot inscriptions matching your query a map.")
 # =========================================================
 # UNIVERSAL CSV EXPORT & RELOCATED GLOBAL MAP BUTTON
 # =========================================================
