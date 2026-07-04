47fc402e12e9877c1454a85932a156c3f8867cfa
Jiatong Wang (Alex) <alexjiatongwang@proton.me>
2026-07-05 00:56:26 +0200

    Update explorer_streamlit.py

--- explorer_streamlit.py
+++ explorer_streamlit.py
@@ -2991,7 +2991,7 @@ with st.expander("Expand/Collapse Interactive Map", expanded=True):
         st.warning("No inscription matched your search")
         
     elif st.session_state.get("map_status") == "unmappable_coordinates":
-        st.warning("No inscriptions matching your search are linked to modern coordinates")
+        st.warning("No inscription matching your search is linked to a set of modern coordinates")
         
     elif st.session_state.get("trigger_map_html"):
         # 1. Standard Info Box (Always on Top)
