cc510948f3fe5bfa9c81039b630fc4f0913c09bf
Jiatong Wang (Alex) <alexjiatongwang@proton.me>
2026-07-06 07:15:22 +0200

    Update explorer_streamlit.py

--- explorer_streamlit.py
+++ explorer_streamlit.py
@@ -506,7 +506,7 @@ with col_s2:
         key="id_report_input",
         on_change=reset_map_and_search_flags
     )
-    st.button("Generate Inscription Report", use_container_width=True, type="primary", on_click=callback_id_search)
+    st.button("Generate Inscription Report(s)", use_container_width=True, type="primary", on_click=callback_id_search)
 
 with col_s3:
     pname_input_var = st.text_input(
