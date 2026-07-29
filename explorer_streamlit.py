daf1b50b4b1680749daa0e95a062a511b6eb0167
Jiatong Wang (Alex) <alexjiatongwang@proton.me>
2026-07-29 13:38:08 +0200

    Update explorer_streamlit.py

--- explorer_streamlit.py
+++ explorer_streamlit.py
@@ -650,32 +650,32 @@ with st.expander("Advanced Search", expanded=False):
     st.markdown("### Advanced Search")
     st.caption("Scroll down and click 'Execute Advanced Search' to search!")
          
-   # st.markdown("#### Text Search")       
-   # f_text = st.text_input(
-       # "Can be combined with Filters | Boolean Logic Operators Allowed:", 
-       # placeholder="e.g. Maximinus AND legatum",
-      # on_change=reset_map_and_search_flags
-   # )
+    st.markdown("#### Text Search")       
+    f_text = st.text_input(
+        "Can be combined with Filters | Boolean Logic Operators Allowed:", 
+        placeholder="e.g. Maximinus AND legatum",
+       on_change=reset_map_and_search_flags
+    )
          
-   # st.caption(
-       # "Use 'Advanced People Search' for queries like PERSON A NOT PERSON B | See Supported logic operators", 
-       # help=(
-           # "**Supported Operators:**\n"
-           # "You can use **AND**, **OR**, and **NOT** in your queries; Other boolean operators are not supported by SQL \n"
-       # )
-  #  )
+    st.caption(
+        "Use 'Advanced People Search' for queries like PERSON A NOT PERSON B | See Supported logic operators", 
+        help=(
+            "**Supported Operators:**\n"
+            "You can use **AND**, **OR**, and **NOT** in your queries; Other boolean operators are not supported by SQL \n"
+        )
+    )
          
-   # text_search_mode = st.radio(
-       # "Text Search Strategy:",
-       # options=[
-           # "Assisted Match",
-           # "Exact Match"
-       # ],
-       # index=0,
-       # key="adv_text_search_mode",
-       # on_change=reset_map_and_search_flags,
-      #  help="'Exact Match' searches your exact string. 'Assisted Match' can find inscriptions containing non-standard spellings of your search term; it also checks whether your search term PARTIALLY matches the name of any identified individuals or groups in the corpus and pulls all inscriptions linked with those individuals or groups. We cannot automatically check all inflected forms of your search term at this moment. The search bar also CANNOT search for PERSON A NOT PERSON B because it does not know which specific person you are referring to with your search term; For such purposes it is recommended to use the ADVANCED PEOPLE SEARCH BELOW."
-   # )
+    text_search_mode = st.radio(
+        "Text Search Strategy:",
+        options=[
+            "Assisted Match",
+            "Exact Match"
+        ],
+        index=0,
+        key="adv_text_search_mode",
+        on_change=reset_map_and_search_flags,
+        help="'Exact Match' searches your exact string. 'Assisted Match' can find inscriptions containing non-standard spellings of your search term; it also checks whether your search term PARTIALLY matches the name of any identified individuals or groups in the corpus and pulls all inscriptions linked with those individuals or groups. We cannot automatically check all inflected forms of your search term at this moment. The search bar also CANNOT search for PERSON A NOT PERSON B because it does not know which specific person you are referring to with your search term; For such purposes it is recommended to use the ADVANCED PEOPLE SEARCH BELOW."
+    )
          
     st.markdown("---")
     st.markdown("#### Filters")
@@ -798,8 +798,8 @@ with st.expander("Advanced Search", expanded=False):
 
     with col_btn1:
         form_payload = {
-            #"text": f_text,
-            #"adv_text_search_mode": text_search_mode,
+            "text": f_text,
+            "adv_text_search_mode": text_search_mode,
             "relevance_index": (
                 "All"
                 if f_rel == "All inscriptions regardless of relevance"
